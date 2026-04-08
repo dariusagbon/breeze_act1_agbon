51fd53425e4c867fc99605ad6ceab8eda36cd844
dariusagbon <dariusagbon>
2026-04-08 22:25:02 +0800

    Your message

--- resources/views/rooms.blade.php
+++ resources/views/rooms.blade.php
@@ -1,3 +1,16 @@
-<div>
-    <!-- The whole future lies in uncertainty: live immediately. - Seneca -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rooms</title>
+</head>
+<body>
+    <div>
+        <nav >
+            <a href="{{ route('dashboard') }}">Back to Dashboard</a>
+        </nav>
+    </div>
+    
+</body>
+</html>
